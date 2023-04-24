ea0e89a2c353e13681345afc5f8c2bfe92ae1842
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-04-23 20:11:58 -0500

    modified profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,29 +2,47 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 function Profile() {
-    const profile = useSelector(state => state.profile)
+    const profile = useSelector(state => state.user)
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch({ type: "FETCH_PROFILE" })
     }, []);
 
+    let mailToProfile = `mailto: ${profile.user_email}`
+    let mailToCompany = `mailto: ${profile.supplier_email}`
 
+    function formatPhoneNumber(phoneNumberString) {
+        var cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+        var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
+        if (match) {
+          var intlCode = (match[1] ? '+1 ' : '');
+          return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
+        }
+        return null;
+      }
 
     return (
-        <>
-            <div>Profile Details</div>
-            {profile.length &&
-                profile.map((profileitem) => {
-                    return (
-                        <>
-                        <h3>{profileitem.first_name} {profileitem.last_name}</h3>
-                        <h3>{profileitem.user_email}</h3>
-                        <h3>{profileitem.user_type}</h3>
-                        </>
-                    )
-                })}
-        </>
+        <div>
+            <h2>Profile Details</h2>
+            <ul>
+            <li>Full Name: {profile.first_name} {profile.last_name}</li>
+            <li>Email: <a href={mailToProfile}>{profile.user_email}</a></li>
+            {profile.user_type === "1" ? <li>Admin Status: Administrator</li> : <li>Admin Status: Not Administrator</li>}
+            <li>Username: {profile.username}</li>
+            {profile.supplier_company_name ? ( 
+                <div>
+            <h2>Supplier Status: Supplier</h2>
+            <li>Supplier Corporate Name: {profile.supplier_company_name}</li>
+            <li>Supplier Address: {profile.supplier_company_address}</li>
+            <li>Supplier Phone: {formatPhoneNumber(profile.supplier_company_phone)}</li>
+            <li>Supplier URL: {profile.supplier_company_url}</li>
+            <li>Supplier Email: <a href={mailToCompany}>{profile.supplier_email}</a></li>
+            </div>
+            ) : null}
+            </ul>
+        </div>
+
     )
 }
 
--- src/redux/sagas/profile.saga.js
+++ src/redux/sagas/profile.saga.js
@@ -3,10 +3,7 @@ import { put, takeEvery } from "redux-saga/effects";
 
 function* fetchProfileAgain() {
   try {
-
     const profile = yield axios.get("/api/items/fetchProfile");
-    console.log('incoming items:', profile.data);
-
     yield put({
       type: "SET_PROFILE",
       payload: profile.data,
