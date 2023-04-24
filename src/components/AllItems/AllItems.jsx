53ba13e425feb7d3246b15f00faa483156422be3
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-04-24 16:00:11 -0500

    advanced search method implemented.

--- src/components/AllItems/AllItems.jsx
+++ src/components/AllItems/AllItems.jsx
@@ -10,7 +10,7 @@ import { Button, TextField, Autocomplete } from '@mui/material';
 function AllItems(props) {
     const dispatch = useDispatch();
     const history = useHistory();
-    const user = useSelector(store => store.user.id)
+    const user = useSelector(store => store.user.user_type)
 
     useEffect(() => {
         dispatch({ type: "FETCH_ALL_ITEMS" })
@@ -20,24 +20,10 @@ function AllItems(props) {
     const allItemsFromStore = useSelector((store) => store.allItems);
     const stockItems = useSelector((store => store.myStock))
 
-    
-    // dispatch - fetchAllItems. -> Move to local state (searchability) -> re-render
-    // run dispatch on page load. Not important atm due to localized speed.
-    // figure out search through local state and render depending on whether search is used?
-    // spike Search bars/applicable methods e.g. Filter.
-
-    // switch to list view -> just link to a diff page for now.
-
-    const clickMyStock = () => {
-        history.push('/mystock')
-    }
+    const clickMyStock = () => { history.push('/mystock') }
 
     const clickItemDetail = (id, stockItems) => {
-        console.log(`StockItems:`, stockItems);
         let inStock = false
-
-        // array.includes modification? .some? 
-        // are loops still higher speed than native methods?
         for (let item of stockItems) {
             if (item.object_id === id) {
                 history.push(`/stockItemDetail/${item.mot_id}`)
@@ -52,70 +38,87 @@ function AllItems(props) {
 
     const [listView, setListView] = useState(false)
     const toggleListView = () => { setListView(!listView) }
-    
+
     // search functionality
     const [searchParam, setSearchParam] = useState('')
-    
+    const searchables = allItemsFromStore.map(item => { return item.part_name })
+
+
+    const filterData = (query, data) => {
+        if (!query) {
+            return data;
+        } else {
+            return data.filter((d) => d.toLowerCase().includes(query));
+        }
+    };
+
+    const dataFiltered = filterData(searchParam, searchables);
+    // end search functionality
+
     return (
         <div>
-            <h3>Search All Items By Name<input placeholder='Part Description' value={searchParam} onChange={(e) => setSearchParam(e.target.value)} /></h3>
-            
-            {listView ? <Button onClick={toggleListView}>See Box View</Button> : <Button onClick={toggleListView}>See List View</Button>}
-            <Button onClick={clickMyStock}>See My Stock</Button>
-
-            {user === 1 ? <AddItemToMasterList /> : null}
-
-            {/* <h2>Import some data?</h2>
-            <button onClick={manualPull}>Manual Data Pull</button> */}
-
-
-            {listView === false ? (
-                <div className='stockContainer'>
-                    {allItemsFromStore.map((item) => {
-                        if (searchParam != ''){
-                        if (item.part_name === searchParam) {
-                        return (
-                            <div className='itemCard' key={item.id}>
-                                <Card sx={{ minWidth: 300 }} id={item.id} onClick={() => clickItemDetail(item.id, stockItems)} >
-                                    <h3>Item name: {item.part_name}</h3>
-                                    <h3>Part# {item.part_number}</h3>
-                                    <h4>Item Lead Time: {item.lead_time_weeks} weeks</h4>
-                                    <h4>Mean Time To Failure: {item.mttf_months} months</h4>
-                                    <h4>Object type: {item.object_type}</h4>
-                                </Card>
+            <TextField
+                id="search-bar"
+                className="text"
+                onChange={(e) => {
+                    setSearchParam(e.target.value);
+                }}
+                value={searchParam}
+                label="Enter Search Term"
+                variant="outlined"
+                placeholder="Search..."
+                size="small"
+            />
+            <div>
+                {listView ? <Button onClick={toggleListView}>See Box View</Button> : <Button onClick={toggleListView}>See List View</Button>}
+                <Button onClick={clickMyStock}>See My Stock</Button>
+            </div>
+            <div>
+                {user === 1 ? <AddItemToMasterList /> : null}
+            </div>
+            <div>
+                {listView === false ? (
+                    <div className="stockContainer">
+                        {dataFiltered.map((d, i) => (
+                            <div key={i} >
+                                {allItemsFromStore.map((item => {
+                                    if (item.part_name === d) {
+                                        return (
+                                            <div className='itemCard' key={item.id}>
+                                                <Card sx={{ minWidth: 300 }} id={item.id} onClick={() => clickItemDetail(item.id, stockItems)} >
+                                                    <h3>Item name: {item.part_name}</h3>
+                                                    <h3>Part# {item.part_number}</h3>
+                                                    <h4>Item Lead Time: {item.lead_time_weeks} weeks</h4>
+                                                    <h4>Mean Time To Failure: {item.mttf_months} months</h4>
+                                                    <h4>Object type: {item.object_type}</h4>
+                                                </Card>
+                                            </div>
+                                        )
+                                    }
+                                }))}
                             </div>
-                        )}
-                    } else {
-                        return (
-                            <div className='itemCard' key={item.id}>
-                                <Card sx={{ minWidth: 300 }} id={item.id} onClick={() => clickItemDetail(item.id, stockItems)} >
-                                    <h3>Item name: {item.part_name}</h3>
-                                    <h3>Part# {item.part_number}</h3>
-                                    <h4>Item Lead Time: {item.lead_time_weeks} weeks</h4>
-                                    <h4>Mean Time To Failure: {item.mttf_months} months</h4>
-                                    <h4>Object type: {item.object_type}</h4>
-                                </Card>
-                            </div>
-                        ) 
-                    }})}
-                </div>)
-                : (
+                        ))}
+                    </div>
+                ) : (
+                    // else if list view === true
                     <ul>
-                        {allItemsFromStore.map((item) => {
-                            return (
-                                <div className='listItem' key={item.id}>
-                                    <li id={item.id}
-                                        onClick={() => clickItemDetail(item.id, stockItems)} >
-                                        Item Name: {item.part_name} / Part # {item.part_number}
-
-                                    </li>
-                                </div>
-                            )
-                        })}
-                    </ul>)}
-            {/* end Map */}
+                        {dataFiltered.map((d, i) => (
+                            <div className="text" key={i} >
+                                {allItemsFromStore.map((item => {
+                                    if (item.part_name === d) {
+                                        return (
+                                            <li className='listItem' key={item.id} sx={{ minWidth: 300 }} id={item.id} onClick={() => clickItemDetail(item.id, stockItems)} >
+                                                Item Name: {item.part_name} / Part # {item.part_number}
+                                            </li>
+                                        )
+                                    }
+                                }))}
+                            </div>
+                        ))}
+                    </ul>
+                )}
+            </div>
         </div>
-
     );
 }
 
