ff1bb4a50bee33f37423e79ed5c081d7ac0a1fe6
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-04-27 13:55:54 -0500

    sidebar testing started

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
-import { useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import MenuIcon from '@mui/icons-material/Menu';
 import { green } from '@mui/material/colors'
 import { Button } from '@mui/material';
+import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from 'react-pro-sidebar';
+
 
 function Nav() {
   const user = useSelector((store) => store.user);
@@ -16,70 +18,95 @@ function Nav() {
     setHamburgerBool(!hamburgerBool)
   }
 
+  const { collapseSidebar } = useProSidebar();
+
 
 
   return (
-    // <div className="nav">
+    <>
 
-      <div className='navbar'>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
-
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <div>
-              <MenuIcon className="toggle" sx={{ color: green[500]}} onClick={toggleBurger} />
-            </div>
-
-            {/* {hamburgerBool ? ( */}
-              <>
-                {/* <Link to="/home"> */}
-                <ul className={`menu-nav${hamburgerBool ? ' show-menu' : ''}`} >
-                <li><a href="/user/"><h2 className="navName">Stock Ticker</h2></a></li>
-                {/* </Link> */}
-              
-                <li><Link className="navLink" to="/mystock/0" onClick={toggleBurger}>
-                  My Stock
-                </Link></li>
-
-                <li><Link className="navLink" to="/allitems/" onClick={toggleBurger}>
-                  All Items
-                </Link></li>
-
-                <li><Link className="navLink" to="/suppliers/0" onClick={toggleBurger}>
-                  Suppliers
-                </Link></li>
-
-                <li><Link className="navLink" to="/profile/" onClick={toggleBurger}>
-                  Profile Details
-                </Link></li>
-
-                <li><Link className="navLink" to="/info" onClick={toggleBurger}>
-                  Info Page
-                </Link></li>
-
-                <li><Link className="navLink" to="/about" onClick={toggleBurger}>
-                  About
-                </Link></li>
-
-                <li><Link to="/user" onClick={toggleBurger}>
-                <Button className="navLink" color="secondary" onClick={() => { dispatch({ type: 'LOGOUT' })}}>Logout</Button>
-                </Link></li>
-                </ul>
-              </>
-            {/* ) : null} */}
-          </>
-        )}
-
-
+      <div style={{ display: 'flex', height: '100%' }}>
+        <Sidebar>
+          <Menu>
+            <MenuItem> Documentation</MenuItem>
+            <MenuItem> Calendar</MenuItem>
+            <MenuItem> E-commerce</MenuItem>
+          </Menu>
+        </Sidebar>
+        <main>
+          <button onClick={() => collapseSidebar()}>Collapse</button>
+        </main>
       </div>
-    // </div>
+
+      {/*     
+<Sidebar>
+  <Menu>
+    <SubMenu label="Charts">
+      <MenuItem> Pie charts </MenuItem>
+      <MenuItem> Line charts </MenuItem>
+    </SubMenu>
+    <MenuItem> Documentation </MenuItem>
+    <MenuItem> Calendar </MenuItem>
+  </Menu>
+</Sidebar> */}
+    </>
+
+    //     <div className='navbar'>
+    //       {/* If no user is logged in, show these links */}
+    //       {!user.id && (
+    //         // If there's no user, show login/registration links
+    //         <Link className="navLink" to="/login">
+    //           Login / Register
+    //         </Link>
+    //       )}
+
+    //       {/* If a user is logged in, show these links */}
+    //       {user.id && (
+    //         <>
+    //           <div>
+    //             <MenuIcon className="toggle" sx={{ color: green[500]}} onClick={toggleBurger} />
+    //           </div>
+
+    //           {/* {hamburgerBool ? ( */}
+    //             <>
+    //               {/* <Link to="/home"> */}
+    //               <ul className={`menu-nav${hamburgerBool ? ' show-menu' : ''}`} >
+    //               <li><a href="/user/"><h2 className="navName">Stock Ticker</h2></a></li>
+    //               {/* </Link> */}
+
+    //               <li><Link className="navLink" to="/mystock/0" onClick={toggleBurger}>
+    //                 My Stock
+    //               </Link></li>
+
+    //               <li><Link className="navLink" to="/allitems/" onClick={toggleBurger}>
+    //                 All Items
+    //               </Link></li>
+
+    //               <li><Link className="navLink" to="/suppliers/0" onClick={toggleBurger}>
+    //                 Suppliers
+    //               </Link></li>
+
+    //               <li><Link className="navLink" to="/profile/" onClick={toggleBurger}>
+    //                 Profile Details
+    //               </Link></li>
+
+    //               <li><Link className="navLink" to="/info" onClick={toggleBurger}>
+    //                 Info Page
+    //               </Link></li>
+
+    //               <li><Link className="navLink" to="/about" onClick={toggleBurger}>
+    //                 About
+    //               </Link></li>
+
+    //               <li><Link to="/user" onClick={toggleBurger}>
+    //               <Button className="navLink" color="secondary" onClick={() => { dispatch({ type: 'LOGOUT' })}}>Logout</Button>
+    //               </Link></li>
+    //               </ul>
+    //             </>
+    //           {/* ) : null} */}
+    //         </>
+    //       )}
+    //     </div>
   );
 }
 
