8c963e89875f8cb529a72cbdd0c6ad3d40fdc4b4
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-05-11 17:07:04 -0500

    Update readme, landing.

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -20,13 +20,7 @@ function LandingPage() {
 
       <div className="grid">
         <div className="grid-col grid-col_8">
-          <p>O Fortuna, Velut luna, Statu variabilis, Semper crescis, Aut decrescis;
-            Vita detestabilis, Nunc obdurat, Et tunc curat, Ludo mentis aciem, Egestatem, Potestatem, Dissolvit ut glaciem.
-            Sors immanis, Et inanis, Rota tu volubilis, Status malus, Vana salus, Semper dissolubilis, Obumbrata, Et velata, Michi quoque niteris;
-            Nunc per ludum, Dorsum nudum, Fero tui sceleris.
-            Sors salutis, Et virtutis, Michi nunc contraria, est affectus, et defectus, semper in angaria.
-            Hac in hora, Sine mora, Corde pulsum tangite;
-            Quod per sortem, Sternit fortem, Mecum omnes plangite!
+          <p>Please create a user to proceed, or select Login.
           </p>
 
         </div>
